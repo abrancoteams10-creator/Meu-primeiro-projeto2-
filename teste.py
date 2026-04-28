42b68b3eb2fd351adb1f9d7b5261bf44f9e94d92
abrancoteams10-creator <abranco.teams10@gmail.com>
2026-04-28 15:11:24 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title('Flamengo Tetra da Libertadores')
-st.write('Música do primeiro tetra: Flamengo é gigante e o zico foi cruel, 81 ganhamos a primeira, o bi com jorge jesus, golaço do gabriel, de canhota ele pegou na veia, em 22 mais um sorriso e com sangue nos olhos, em guayaquil o mrngão foi tri, em 25 era meta, ganhar o primeiro tetra e a nação se divertir, bh levanta a taça, o jorginho esculacha, o flamengo não é brincadeira, pelo rossi nada passa, cruzamento do arrasca e o gol do danilo de cabeça, é TETRA AA, é TETRA AA, é TETRA, primeiro tetra da liberta')
+st.write('Música do primeiro tetra: Flamengo é gigante e o zico foi cruel, 81 ganhamos a primeira, o bi com jorge jesus golaço do gabriel de canhota ele pegou na veia, em 22 mais um sorriso e com sangue nos olhos em guayaquil o mengão foi tri, em 25 era meta, ganhar o primeiro tetra e a nação se divertir, bh levanta a taça, o jorginho esculacha, o flamengo não é brincadeira, pelo rossi nada passa, cruzamento do arrasca e o gol do danilo de cabeça, é TETRA AA, é TETRA AA, é TETRA, primeiro tetra da liberta! MENGOOOOOOO <3')
